f0c80bde963b59172d88bbaae0b7cbb840ccc81c
Nonni Kosmos <jon@juni.is>
2026-03-07 10:43:45 +0000

    fix build

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import * as prismic from "@prismicio/client";
 import { createClient } from "@/prismicio";
 import { SliceZone } from "@prismicio/react";
 import { components } from "@/slices";
@@ -10,10 +11,15 @@ export default async function Home() {
   const client = createClient();
   const homepage = await client.getSingle("homepage");
   const pageSettings = await client.getSingle("page_settings");
+  const screensaverUrl = prismic.isFilled.linkToMedia(
+    pageSettings.data.screensaver,
+  )
+    ? pageSettings.data.screensaver.url
+    : null;
 
   return (
     <div className="min-h-screen">
-      <ScreensaverOverlay videoUrl={pageSettings.data.screensaver?.url} />
+      <ScreensaverOverlay videoUrl={screensaverUrl} />
       <Logo />
       <SliceZone slices={homepage.data.slices} components={components} />
       <About
